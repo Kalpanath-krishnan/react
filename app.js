0ceb4c542b65d73491fe2e423a54446f696d121e
kalpanath_k <knkrish1991@gmail.com>
2024-01-30 09:21:15 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+const heading = React.createElement("div",{id:"parent"},[(React.createElement("div",{id:"child"},[(React.createElement("h1",{id:"child"},"I am a H1 tag")),(React.createElement("h2",{id:"child"},"I am a H2 tag"))])),(React.createElement("div",{id:"child"},[(React.createElement("h1",{id:"child"},"I am a H1 tag")),(React.createElement("h2",{id:"child"},"I am a H2 tag"))]))]);
+const root = ReactDOM.createRoot(document.getElementById("main"));
+root.render(heading);
+console.log(heading);
